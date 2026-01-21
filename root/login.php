81d67bc5c3e8b4ec6cc24e93b93ed49855073246
Resen08 <jiwon20060811@gmail.com>
2026-01-21 18:52:35 +0100

    21.1.2026

--- root/login.php
+++ root/login.php
@@ -3,14 +3,4 @@ session_start();
 
 include_once('../classes/DatabaseConnection.class.php');
 $dbc = new DatabaseConnection();
-session_destroy();
 
-if ($dbc->login(filter_input(INPUT_POST, "user", FILTER_SANITIZE_STRING), filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING))) {
-   print "Login successful!";
-   
-} else {
-    print 
-    "Irgendwas stimmt hier nicht!<br>
-    <a href='../index.html'>Zurück zum Login</a>
-    ";
-}
---
+++ root/regi.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+include_once('../classes/DatabaseConnection.class.php');
+$dbc = new DatabaseConnection();
+
+if(isset($_SESSION['token'])){
+    print "Sie sind bereits eingeloggt!<br>";
+    exit;
+}
+if ($dbc->login(filter_input(INPUT_POST, "user", FILTER_SANITIZE_STRING), filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING))) {
+    session_unset();
+    print "Sie sind eingeloggt!";
+    $_SESSION['token'] = "Du bist eingeloggt";
+   
+} else {
+    session_unset();
+    print 
+    "Irgendwas stimmt hier nicht!<br>
+    <a href='../index.html'>Zurück zum Login</a>
+    ";
+}
