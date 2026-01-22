4e09e74682863fe7e30235ffb7de0b23a5f7564e
Resen08 <jiwon20060811@gmail.com>
2026-01-22 14:05:20 +0100

    abgabe

---
+++ classes/HTMLPage.class.php
@@ -0,0 +1,26 @@
+<?php
+
+class HTMLPage{
+
+public function head($title){
+    return '<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <title>'.$title.'</title>
+    </head>
+    <body>
+        <div class="container">
+    ';
+}
+
+public function foot(){
+    return '
+    </div>
+        <script src="js/bootstrap.bundle.min.js"></script>
+    </body>
+    </html>';
+}
+}
