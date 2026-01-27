0ccbc652bcfbeca3a03e9b264070aa5caee8078c
Resen08 <Jiwon20060811@gmail.com>
2026-01-27 20:44:22 +0100

    abgabe

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ print '
             <p><input type="password" id="password" name="password" placeholder="Passwort"></p>
             <p><input type="submit" id="senden" name="senden" value="login"></p>
         </form>
+        <a href="index.php?haha">Registrieren</a>
     </section>
     <div id="info">
         <img src="img/vibebelike.webp" alt="Vibe Be Like">
