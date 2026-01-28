eb735cb1b4a3afc838a0cdbbb6d94070aa5402c5
Resen08 <Jiwon20060811@gmail.com>
2026-01-28 23:18:42 +0100

    abgabe

--- classes/News.class.php
+++ classes/News.class.php
@@ -19,7 +19,38 @@ class News{
                 <div class='postUpload'>
                 </div>";
 
-        
+        $postStmt = "SELECT * FROM posts";
+        $post = $this->db->query($postStmt);
+        while($post && $row = $post->fetch_assoc()){
+            $head .= "
+                    <div class='postUploaded'>
+                        <div class='postuploadedHeader'>
+                            <div class='postLeft'>
+                    ";
+
+                    $userStmt = "SELECT user From userDB WHERE uid='$row[uid]'";
+                    $user = $this->db->query($userStmt);
+                    while($user && $rows = $user->fetch_assoc()){
+                    $head .="
+                            <span class='uploader'>".$rows['user']."</span>
+                        ";
+                    }
+
+            $head .="
+                        
+                                <span class='postTitle'>".$row['title']." </span>
+                            </div>
+                            <div class='postRight'>
+                                <span class='created'> ".$row['created']." </span>
+                            </div>
+                        </div>
+                        <br>
+                        <span class='postContent'>".$row['content']."</span>
+
+                    </div>
+                ";
+        }
+
 
         $head .= "</div>
             <div class='userPanel'>
