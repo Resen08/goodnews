4bb0a5d10a657c2e1fe49236d1d8acc1f9f12f0a
Resen08 <Jiwon20060811@gmail.com>
2026-01-29 00:16:17 +0100

    abgabe

--- classes/News.class.php
+++ classes/News.class.php
@@ -19,7 +19,7 @@ class News{
                 <div class='postUpload'>
                 </div>";
 
-        $postStmt = "SELECT * FROM posts";
+        $postStmt = "SELECT * FROM posts WHERE parent_id IS NULL";
         $post = $this->db->query($postStmt);
         while($post && $row = $post->fetch_assoc()){
             $head .= "
@@ -37,7 +37,6 @@ class News{
                     }
 
             $head .="
-                        
                                 <span class='postTitle'>".$row['title']." </span>
                             </div>
                             <div class='postRight'>
@@ -46,7 +45,42 @@ class News{
                         </div>
                         <br>
                         <span class='postContent'>".$row['content']."</span>
+                        <br><br><h3>Kommentare:</h3>
+            ";
 
+            $kommentstmt = "SELECT * FROM posts WHERE parent_id='$row[pid]'";
+            $kom = $this->db->query($kommentstmt);
+            while($kom && $rowss = $kom->fetch_assoc()){
+                $head .="
+                    <div class='kommentare'>                        
+                        <div class='postuploadedHeader'>
+                            <div class='postLeft'>
+                    ";
+                $kommentuserstmt = "SELECT user From userDB WHERE uid='$rowss[uid]'";
+                $komuser = $this->db->query($kommentuserstmt);
+                while($komuser && $rowsss = $komuser->fetch_assoc()){
+                    $head .="
+                                <span class='uploader'>".$rowsss['user']."</span>
+                            ";
+                }
+                $head .="
+                                    <span class='postTitle'>".$rowss['title']." </span>
+                                </div>
+                                <div class='postRight'>
+                                    <span class='created'> ".$rowss['created']." </span>
+                                </div>
+                            </div>
+                            <br>
+                            <span class='postContent'>".$rowss['content']."</span>
+                            
+                ";
+
+
+                $head .="</div>";
+            }
+
+            $head .="
+                        
                     </div>
                 ";
         }
